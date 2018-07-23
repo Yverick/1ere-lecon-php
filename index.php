5508180bcc22dd8cceaa5396d613c9cb68cacd1e
Yvé <40633415+Yverick@users.noreply.github.com>
2018-07-23 12:23:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+// Exercice 1 = Variable "name"
+$name= 'Yverick DA COSTA';
+echo "$name";
+
+// Exercice 2 = Variables : "First name", "Last name" & "Age"
+$firstname =  "Yverick";
+echo "$firstname";
+
+$lastname =  "DA COSTA";
+echo "$lastname";
+
+$age = "23";
+echo "$age";
+
+// Exercice 3 = Variable "km"
+$km = 1;
+echo "$km";
+ 
+$km = 3;
+echo "$km";
+
+$km = 125;
+echo "$km";
+
+// Exercice 4 = Creation variables string/int/float/booléan
+// String
+$firstname = "Yverick";
+echo "$firstname";
+
+// Int
+$age = 23;
+echo "$age";
+
+// Float
+$decimal = 52.68;
+echo "$decimal";
+
+// Booléan
+$bool = "true";
+echo "$bool";
+
+// Exercice 5 = Creation variable int avec valeur nulle
+$nombre = null;
+echo "$nombre";
+
+// Exercice 6 = Créer une variable name + concatenation
+$name = "JeanJean";
+echo   "Bonjour " . $name . ", comment vas tu ?";
+
+// Exercice 7 = 
+$lastname = "Maurice";
+$firstname = "Jean";
+$age = 156;
+echo "Bonjour " . $lastname . $firstname . ", tu as " . $age . "ans";
+
+// Exercice 8 = Calculs 
+$addition = 3+4;
+echo $addition;
+
+$multiplication = 5*20;
+echo $multiplication;
+
+$division = 45/5;
+echo $division;
